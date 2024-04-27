8965100b284e738bab3f152e875e29a4ebcd5b86
adrian-reina-391 <55458805+adrian-reina-391@users.noreply.github.com>
2024-04-26 22:01:52 -0400

    _DownloadPDF_CV

--- CV/assets/scripts/main.js
+++ CV/assets/scripts/main.js
@@ -1,19 +1,22 @@
 const element = document.body;
-        const options = {
-            filename: 'Adrian_Reina_CV.pdf',
-            html2canvas: {
-                scale: 2
-            },
-            exclude: [
-                '#downloadPdfBtn', // Excluir el botón de descarga del PDF
-                '.switch' // Excluir el switch de modo oscuro
-            ]
-        };
+const options = {
+    filename: 'Adrian_Reina_CV.pdf',
+    html2canvas: {
+        scale: 3 // Aumentar la escala
+    },
+    exclude: [
+        '#downloadPdfBtn', // Excluir el botón de descarga del PDF
+        '.switch' // Excluir el switch de modo oscuro
+    ],
+    pagebreak: { mode: 'avoid-all' }, // Evitar división de hojas
+    jsPDF: {
+        format: 'a4', // Establecer el formato del documento PDF
+        orientation: 'portrait' // Establecer la orientación del documento
+    }
+};
 
-        const downloadBtn = document.getElementById('downloadPdfBtn');
+const downloadBtn = document.getElementById('downloadPdfBtn');
 
-        downloadBtn.addEventListener('click', function() {
-            html2pdf().from(element).set(options).save();
-        });
-
-        
+downloadBtn.addEventListener('click', function() {
+    html2pdf().from(element).set(options).save();
+});
